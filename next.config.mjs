1683d0ebf294d83bfef2a202d6412d45a57ebf17
abudev0 <abdulazizbekabdusodiqov@gmail.com>
2026-08-20 19:04:22 +0500

    feat: add redirect configuration for www.mindera.uz to https://mindera.uz

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.mindera.uz' }],
+        destination: 'https://mindera.uz/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
